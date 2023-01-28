41d61344cb1a4b35921c0d24c0dd7fe25816f2af
kmk00 <73724318+kmk00@users.noreply.github.com>
2023-01-28 17:25:24 +0100

    Fix bugs

--- components/Main.js
+++ components/Main.js
@@ -3,17 +3,17 @@ import React from "react";
 function MainSection({ weather }) {
   return (
     <>
-      <main className="md:w-3/5 h-screen relative">
+      <main className="md:w-3/5 md:h-screen relative">
         {weather && (
-          <div className="flex px-28 py-12 m-4 gap-2 items-center md:absolute md:bottom-10 md:left-10 rounded-xl align-bottom bg-black/30">
-            <div className="flex flex-col justify-center items-center ">
-              <p className="text-4xl">{weather.name}</p>
-              <img
-                className="h-32 w-32"
-                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              ></img>
-            </div>
-            <p className="text-8xl p-2">{Math.round(weather.main.temp)}°C</p>
+          <div className="flex flex-col px-1 py-4 m-4 gap-2 items-center justify-center md:absolute md:bottom-10 md:left-10 md:px-32 rounded-xl align-bottom bg-black/30">
+            <p className="md:text-4xl text-3xl">{weather.name}</p>
+            <img
+              className="h-32 w-32"
+              src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            ></img>
+            <p className="md:text-8xl text-5xl p-2">
+              {Math.round(weather.main.temp)}°C
+            </p>
           </div>
         )}
       </main>
